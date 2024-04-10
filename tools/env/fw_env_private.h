f74cc90a59980a07a58c9a65ca62381cb9401078
Arthur Crepin-Leblond <arthur.crepin@proglove.de>
2024-04-10 10:20:23 +0200

    tools: Disable the lock for printenv

--- tools/env/fw_env_private.h
+++ tools/env/fw_env_private.h
@@ -21,20 +21,20 @@
  * See included "fw_env.config" sample file
  * for notes on configuration.
  */
-#define CONFIG_FILE     "/etc/fw_env.config"
+/* #define CONFIG_FILE     "/etc/fw_env.config" */
 
 #ifndef CONFIG_FILE
-#define HAVE_REDUND /* For systems with 2 env sectors */
-#define DEVICE1_NAME      "/dev/mtd1"
-#define DEVICE2_NAME      "/dev/mtd2"
-#define DEVICE1_OFFSET    0x0000
-#define ENV1_SIZE         0x4000
-#define DEVICE1_ESIZE     0x4000
-#define DEVICE1_ENVSECTORS     2
-#define DEVICE2_OFFSET    0x0000
-#define ENV2_SIZE         0x4000
-#define DEVICE2_ESIZE     0x4000
-#define DEVICE2_ENVSECTORS     2
+/* #define HAVE_REDUND /\* For systems with 2 env sectors *\/ */
+#define DEVICE1_NAME      "/dev/mmcblk0"
+/* #define DEVICE2_NAME      "/dev/mtd2" */
+#define DEVICE1_OFFSET    CONFIG_ENV_OFFSET
+#define ENV1_SIZE         CONFIG_ENV_SIZE
+/* #define DEVICE1_ESIZE     0x4000 */
+/* #define DEVICE1_ENVSECTORS     2 */
+/* #define DEVICE2_OFFSET    0x0000 */
+/* #define ENV2_SIZE         0x4000 */
+/* #define DEVICE2_ESIZE     0x4000 */
+/* #define DEVICE2_ENVSECTORS     2 */
 #endif
 
 #ifndef CONFIG_BAUDRATE
